20a760b88daa9ac46189e193c273329df94ac6bd
Stephen Mizell <smizell@gmail.com>
2012-02-15 09:07:28 -0600

    Added bootstrap to project

--- kate3/urls.py
+++ kate3/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
@@ -13,3 +14,8 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('userena.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
