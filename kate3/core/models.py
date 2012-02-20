d4391625f24ddd70e7a0d7a9de39ad02c42d2171
Stephen Mizell <smizell@gmail.com>
2012-02-20 13:28:01 -0600

    Moved the UserProfile to the new accounts app

--- kate3/core/models.py
+++ kate3/core/models.py
@@ -1,11 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from userena.models import UserenaBaseProfile
-
-class UserProfile(UserenaBaseProfile):
-    user = models.OneToOneField(User)
-    levels = models.ManyToManyField('Level', blank=True)
-    content_areas = models.ManyToManyField('ContentArea', blank=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
