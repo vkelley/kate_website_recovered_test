c3602c75c236dc9a4a57e38591c5f32c9f9621c5
Stephen Mizell <smizell@gmail.com>
2012-02-29 14:25:42 -0600

    Added news to the home page

--- kate3/pages/views.py
+++ kate3/pages/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from logger.models import Entry
-from tick.models import Notice
+from tick.models import Announcement, Notice
 
 def home(request):
     recent_resources = Entry.objects.get_for_model('tick', 'resource', action='Published')[0:4]
     tick_prize = Notice.objects.latest('created_at')
+    tick_news = Announcement.objects.latest('created_at')
     return render_to_response('pages/home.haml',
-                              {'recent_resources': recent_resources, 'tick_prize': tick_prize},
+                              {'recent_resources': recent_resources, 'tick_prize': tick_prize,
+                               'tick_news': tick_news},
                               context_instance=RequestContext(request))
 
 def about(request):
