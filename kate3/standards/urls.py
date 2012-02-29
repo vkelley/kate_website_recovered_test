bc211451d21c846590ae2a661fa69fb2bf4004a7
Stephen Mizell <smizell@gmail.com>
2012-02-29 09:49:23 -0600

    Added standards index

--- kate3/standards/urls.py
+++ kate3/standards/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$',
-		'standards.views.index'),
+		'standards.views.index',
+		name='standards_index'),
+
+	url(r'^core_content/(?P<subject>[A-Z]{2})/$',
+		'standards.views.core_content',
+		name='standards_corecontent'),
 )
--- kate3/standards/views.py
+++ kate3/standards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from tick.models import CoreContent
+
 CORE_CONTENT_LIST = {
     'AH': {'id': 5, 'title': 'Arts &amp; Humanities'},
     'MA': {'id': 1, 'title': 'Mathematics'},
@@ -13,12 +15,13 @@ CORE_CONTENT_LIST = {
 
 def index(request):
     return render_to_response('standards/index.haml',
+                              {'core_contents': CoreContent.objects.all(),},
                               context_instance=RequestContext(request))
 
 def core_content(request, subject):
     core_contents = CoreContent.objects.filter(subject__exact=CORE_CONTENT_LIST[subject]['id']).order_by('level','bigidea','subcategory','catpoint')
     
-    return render_to_response('kate/core_content_subject.html',
+    return render_to_response('standards/core_content.haml',
                               {'core_contents': core_contents, 
                                'title': CORE_CONTENT_LIST[subject]['title']},
                               context_instance=RequestContext(request))
