4403a7d1ff3ff10bb6de0d86c05fe111f896ef76
Stephen Mizell <smizell@gmail.com>
2012-02-29 13:40:59 -0600

    Added view and template for home

--- kate3/pages/views.py
+++ kate3/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+def home(request):
+	return render_to_response('pages/home.haml',
+							  context_instance=RequestContext(request))
+
 def about(request):
 	return render_to_response('pages/about.haml',
 							  context_instance=RequestContext(request))
--- kate3/urls.py
+++ kate3/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^about/$', 'pages.views.about', name='pages_about'),
     url(r'^contact/$', 'pages.views.contact', name='pages_contact'),
     url(r'^tis/$', 'pages.views.tis', name='pages_tis'),
+    url(r'^/$', 'pages.views.home', name='home'),
 
     url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
