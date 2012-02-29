112d0d54381f278ab851bd8ffa0437adc19dad5c
Stephen Mizell <smizell@gmail.com>
2012-02-29 10:06:05 -0600

    Added views for pages

--- kate3/pages/views.py
+++ kate3/pages/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render_to_response
 
+def about(request):
+	return render_to_response('pages/about.haml',
+							  context_instance=RequestContext(request))
+
+def contact(request):
+	return render_to_response('pages/contact.haml',
+							  context_instance=RequestContext(request))
+
+def tis_websites(request):
+	return render_to_response('pages/tis.haml',
+							  context_instance=RequestContext(request))
+
 def robot(request):
 	return render_to_response('pages/robots.txt', mimetype='text/plain')
