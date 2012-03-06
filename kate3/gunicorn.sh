ad23d84027e4075ddf9d1e6c4ecda42e239b7ce9
Stephen Mizell <smizell@gmail.com>
2012-03-06 12:04:21 -0600

    Added gunicorn script

---
+++ kate3/gunicorn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  set -e
+  LOGFILE=/var/log/gunicorn/hello.log
+  LOGDIR=$(dirname $LOGFILE)
+  NUM_WORKERS=3
+  # user/group to run as
+  USER=apache
+  GROUP=web
+  cd /web/projects/kate3base/src/kate3
+  source ../../bin/activate
+  test -d $LOGDIR || mkdir -p $LOGDIR
+  exec ../../bin/gunicorn_django -w $NUM_WORKERS \
+    --user=$USER --group=$GROUP --log-level=debug \
+    --log-file=$LOGFILE 2>>$LOGFILE
