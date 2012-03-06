f38c0f0ec6f3410c54090eb7092de737263fce74
Stephen Mizell <smizell@coekate.murraystate.edu>
2012-03-06 13:59:04 -0600

    Changed some settings in the .sh file

--- kate3/gunicorn.sh
+++ kate3/gunicorn.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
   set -e
-  LOGFILE=/var/log/gunicorn/hello.log
+  LOGFILE=/var/log/gunicorn/kate3.log
   LOGDIR=$(dirname $LOGFILE)
   NUM_WORKERS=3
   # user/group to run as
-  USER=apache
-  GROUP=web
+  USER=smizell
+  GROUP=smizell
+  cd /web/projects/kate3base
+  source bin/activate
   cd /web/projects/kate3base/src/kate3
-  source ../../bin/activate
   test -d $LOGDIR || mkdir -p $LOGDIR
-  exec ../../bin/gunicorn_django -w $NUM_WORKERS \
+  exec /web/projects/kate3base/bin/gunicorn_django -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
