5077f241d45dfc334c79f49bb5d81736e915e767
Stephen Mizell <smizell@gmail.com>
2012-03-14 09:17:44 -0500

    Fixed up admin site to do some calls to the API

--- kate3/static/js/admin.js
+++ kate3/static/js/admin.js
@@ -1,14 +1,36 @@
 $ = django.jQuery;
 
-$(document).ready(function() {
+set_tech_standards = function(){
+    $('#tech_standard_list').html("");
     standards_select = $('select#id_tech_standards');
 
-    $.get("/api/v1/tech_standard/", {'id[]': standards_select.val()},
-        function(data) {
+    $.get("/api/v1/tech_standard/set/"+standards_select.val().join(";")+"/", {},
+        function(data){
+            
+            $('<table/>')
+                .attr('id', 'tech_standards_table')
+                .attr('style', 'border: 1px solid #eee; border-width: 1px 1px 0 1px;')
+                .appendTo('#tech_standard_list');
+
             $.each(data.objects, function(index, object) {
-                console.log(object.category);
+                $('#tech_standards_table')
+                    .append("<tr><td width='6%'><strong>"+object.name+"</strong></td><td>"+object.description+"</td></tr>")
             });
         });
+}
 
+tech_standards = function(){
+    $('<div/>')
+        .attr("id", "tech_standard_list")
+        .appendTo('.tech_standards');
 
+    set_tech_standards();
+
+    $('select#id_tech_standards').change(function() {
+        set_tech_standards();
+    });
+};
+
+$(document).ready(function(){
+    tech_standards();
 });
