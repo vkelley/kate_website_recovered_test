8c721ad634d426a0c69ba55935f4c019b605d214
Stephen Mizell <smizell@gmail.com>
2012-03-27 14:50:50 -0500

    Now looks up iTunes up when saving

--- kate3/mobile_apps/forms.py
+++ kate3/mobile_apps/forms.py
@@ -5,7 +5,7 @@ from mobile_apps.models import App
 class AppForm(forms.ModelForm):
     class Meta:
         model = App
-        fields = ('name', 'description', 'type', 'cost', 'levels', 'content_areas')
+        fields = ('name', 'educational_uses', 'type', 'cost', 'levels', 'content_areas')
 
     def save(self, user, *args, **kwargs):
         app = super(AppForm, self).save(commit=False, *args, **kwargs)
